fdf3a3fe7fba965f36b7b78add7618d8243e8a7b
Leonardo Macedo <leo.depaula2019@gmail.com>
2023-04-03 21:56:35 -0300

    throw exceções

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-        System.out.println("Teste");
+        int[][] matriz = {{1, 2}, {3,4}, {5, 6}}; //3 linhas e 2 colunas
+        int[][] novaMatriz = new int[2][3]; //2 linhas e 3 colunas
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
+                novaMatriz[j][i] = matriz [i][j];
+            }
+        }
+
+        for (int i = 0; i < 2; i++) {
+            System.out.println();
+            for (int j = 0; j < 3; j++) {
+                System.out.print(novaMatriz[i][j] + " ");
+            }
+        }
     }
 }
